40275eb5b7b2d8a8b5b6e2e49e5d857aca5dbe13
Maksim Patrakeev <patrakeev119@gmail.com>
2021-04-07 00:30:54 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        boolean registered = true;
         int TicketPrice = 20;
         int Bonus = 1;
         int TotalBonus = TicketPrice / Bonus;
         int FloatBonus = (int) TotalBonus;
-
         System.out.println(FloatBonus);
     }
 }
